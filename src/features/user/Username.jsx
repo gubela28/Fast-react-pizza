d6cd3bb0271b8771f9ee9b020c25f3f99b3c8606
gubela28 <lukagubelidze28@gmail.com>
2023-10-05 16:35:50 +0400

    adding some styles

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Useraneme() {
+  return <div className="text-sm font-semibold hidden md:block">Luka</div>;
+}
+
+export default Useraneme;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Useraneme from "../features/user/Username";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 flex items-center justify-between uppercase px-4 py-3 border-b border-stone-200 sm:px-6">
+      <Link to="/" className="tracking-widest text-base font-medium">
+        Fast React Pizza Co.
+      </Link>
 
       <SearchOrder />
 
-      <p>pizza</p>
+      <Useraneme />
     </header>
   );
 }
